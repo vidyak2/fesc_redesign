02ece87d82fbfba6bb09fb7aa32bcabe31b4655b
vidyak2 <vidyakrishnan.vk@gmail.com>
2018-04-25 09:09:56 -0700

    Add video, map link, and correct errors

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+//Attempt to create a Google Map link for contact.html
+//function myMap() {
+//  var mapOptions = {
+//    center: new google.maps.LatLng(29.649277, -82.341972),
+//    zoom: 10,
+//    mapTypeId: google.maps.MapTypeId.ROADMAP
+//  }
+//  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//}
