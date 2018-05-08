4073504bb44a887db53b3810015acffc57058018
vidyak2 <vidyakrishnan.vk@gmail.com>
2018-05-08 13:12:40 -0700

    Update script.js in master

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-//Attempt to create a Google Map link for contact.html
-//function myMap() {
-//  var mapOptions = {
-//    center: new google.maps.LatLng(29.649277, -82.341972),
-//    zoom: 10,
-//    mapTypeId: google.maps.MapTypeId.ROADMAP
-//  }
-//  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-//}
+function openNav() {
+  document.getElementById("reducedNav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("reducedNav").style.width = "0";
+}
