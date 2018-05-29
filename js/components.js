fbdab9e7f786f7c419e1a86c8a1fcbc6b28bd918
vidyak2 <vidyakrishnan.vk@gmail.com>
2018-05-29 14:13:33 -0700

    Add charts.js and correct logo in index.html

--- js/components.js
+++ js/components.js
@@ -2,15 +2,15 @@
 
 Vue.component('app-header', {
   props: ['text'],
-  template: '<header><h1><img src="images/logo.png" alt="logo">{{text}}</h1></header>'
+  template: '<header><h1>{{text}}</h1></header>'
 })
 
 Vue.component('app-second-header', {
   props: ['text'],
-  template: '<header><h2>{{text}}</h2></header>'
+  template: '<h2>{{text}}</h2>'
 })
 
 Vue.component('app-third-header', {
   props: ['text'],
-  template: '<header><h3>{{text}}</h3></header>'
+  template: '<h3>{{text}}</h3>'
 })
--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+
   }
 })
