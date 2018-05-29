918066dc254056689523529b89189c598981d153
vidyak2 <vidyakrishnan.vk@gmail.com>
2018-05-29 08:02:18 -0700

    Convert logo to vue.js

--- js/components.js
+++ js/components.js
@@ -1,14 +1,16 @@
-// Making the header dynamically change, as a component
+// Logo 
 
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+Vue.component('app-header', {
+  props: ['text'],
+  template: '<header><h1><img src="images/logo.png" alt="logo">{{text}}</h1></header>'
 })
 
-//
-//Vue.component('app-header', {
-//  props: ['text'],
-//  template: '<header><h1><img src="images/logo.png" alt="logo">{{text}}</h1></header>'
-//})
+Vue.component('app-second-header', {
+  props: ['text'],
+  template: '<header><h2>{{text}}</h2></header>'
+})
+
+Vue.component('app-third-header', {
+  props: ['text'],
+  template: '<header><h3>{{text}}</h3></header>'
+})
---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Making the header dynamically change, as a component
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
