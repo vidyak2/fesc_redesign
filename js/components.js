f25f7e88b14d44b713fc8662b9493f1edf12528a
vidyak2 <vidyakrishnan.vk@gmail.com>
2018-05-29 17:12:00 -0700

    Vue.js header working in index.html and index.js

--- js/components.js
+++ js/components.js
@@ -1,16 +1,18 @@
 // Logo 
 
 Vue.component('app-header', {
-  props: ['text'],
-  template: '<header><h1>{{text}}</h1></header>'
+  //What is accepted
+  props: ['text', 'texttwo', 'textthree'],
+  //How it will appear
+  template: '<header><nav class="mainnav"><div class="drpdwn"><form action="index.html"><input class="drpbtn" type="submit" value="Home"></form><div class="drpdwn-cntent"><a href="#">FESC Expertise</a><a href="#">Technology Commercialization</a><a href="#">Annual Reports, Publications and Presentations</a><a href="#">Advisory Board</a><a href="#">Supplementary Information</a></div></div><div class="drpdwn"><form action="research.html"><input class="drpbtn" type="submit" value="FESC Research"></form><div class="drpdwn-cntent"><a href="#">Research Archives</a></div></div><div class="drpdwn"><form action="energyindustry.html"><input class="drpbtn" type="submit" value="Energy Industry"></form><div class="drpdwn-cntent"><a href="#">Florida Energy Facts</a><a href="#">Energy Efficiency</a><a href="#">Energy Crops</a><a href="#">Waste to Energy</a><a href="#">Smart Grid & Power</a><a href="#">Energy Storage</a><a href="#">Network & Security</a><a href="#">Water Purification</a><a href="#">Consulates & Law</a><a href="#">Engineering Consulting</a><a href="#">Associations & Organizations</a><a href="#">Energy Financing & Funding</a><a href="#">FL University Spin-off</a><a href="#">Other</a></div></div><a href="charts.html">Data and Charts</a><a href="contact.html">Contact Us</a></nav><div class="top"><div class="headerimg"><span class="open" onclick="openNav()"><i class="fas fa-bars"></i></span><img src="images/logo.png" alt="logo"><article class="center"><form name="research" onsubmit="search()"><label>Search:<input class="topsearch" type="text" name="searchbar"></label></form></article></div><div class="logotext"><div class="beggining"><h1>{{ text }}</h1></div><div class="below"><h2>{{ texttwo }}</h2><h3>{{ textthree }}</h3></div></div></div></header>'
 })
 
-Vue.component('app-second-header', {
-  props: ['text'],
-  template: '<h2>{{text}}</h2>'
-})
-
-Vue.component('app-third-header', {
-  props: ['text'],
-  template: '<h3>{{text}}</h3>'
-})
+//Vue.component('app-second-header', {
+//  props: ['text'],
+//  template: '<h2>{{text}}</h2>'
+//})
+//
+//Vue.component('app-third-header', {
+//  props: ['text'],
+//  template: '<h3>{{text}}</h3>'
+//})
--- js/energyindustry.js
+++ js/energyindustry.js
@@ -1,7 +1,7 @@
 //Call Vue function
 var app = new Vue({
   //What Vue is going to dynamically change
-  el: '#app',
+  el: '#appEnergy',
   data: {
 
   }
